7385da77841da37fdd19c510e1bd45827306784e
slwangm <slwangm@isoftstone.com>
2018-06-04 11:51:45 +0800

    按用户查询

--- erlangshen/src/main/java/com/website/dao/PermissionRoleDao.java
+++ erlangshen/src/main/java/com/website/dao/PermissionRoleDao.java
@@ -1,13 +1,23 @@
 package com.website.dao;
 
+import com.website.model.vo.PermissionRoleVO;
 import org.springframework.stereotype.Repository;
 import com.fastjavaframework.base.BaseDao;
 import com.website.model.bo.PermissionRoleBO;
+
+import java.util.List;
 
 /**
  * @author https://github.com/shuli495/erlangshen
  */
 @Repository
-public class PermissionRoleDao extends BaseDao<PermissionRoleBO> {
+public class PermissionRoleDao extends BaseDao<PermissionRoleVO> {
 
+    /**
+     * 按用户id查询角色
+     * @return
+     */
+    public List<PermissionRoleVO> queryByUser(String userId) {
+        return this.sql().selectList("queryByUser", userId);
+    }
 }
